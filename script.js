adedd34a8b0eecd9ce9bae118617f2aedc697d42
Nimmy <knimmy04@gmail.com>
2024-04-28 21:57:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentQuestion = 0;
+let Score = 0;
+let highScores =0;
+let userName = "";
+let quizData = [];
+let SelectedOption = "";
+
+
+
+let highScoreBtn = document.getElementById("highscore-btn");
+highScoreBtn,addEventListener("click", (event) => {
+    document.getElementById("highscore-page").style.display = "block";
+    document.getElementById("start-page").style.display = "none";
+}
+let startQuiz = document.getElementById("start-btn");
+startQuiz,addEventListener("click", (event) => {
+    document.getElementById("").style.display = "block";
+    document.getElementById("start-page").style.display = "none";
+}
